9d39053ba5310a6dc94e6ad8dc6e2943b03c3f4a
vtears <772326244@qq.com>
2019-02-14 10:52:05 +0800

    修改iPhoneX的判断

--- WRNavigationBar/WRNavigationBar.m
+++ WRNavigationBar/WRNavigationBar.m
@@ -10,20 +10,22 @@
 #import "WRNavigationBar.h"
 #import <objc/runtime.h>
 #import "sys/utsname.h"
+#import "SWPhoneInfo.h"
 
 @implementation WRNavigationBar
 
 + (BOOL)isIphoneX {
-    struct utsname systemInfo;
-    uname(&systemInfo);
-    NSString *platform = [NSString stringWithCString:systemInfo.machine encoding:NSASCIIStringEncoding];
-    if ([platform isEqualToString:@"i386"] || [platform isEqualToString:@"x86_64"]) {
-        // judgment by height when in simulators
-        return (CGSizeEqualToSize([UIScreen mainScreen].bounds.size, CGSizeMake(375, 812)) ||
-                CGSizeEqualToSize([UIScreen mainScreen].bounds.size, CGSizeMake(812, 375)));
-    }
-    BOOL isIPhoneX = [platform isEqualToString:@"iPhone10,3"] || [platform isEqualToString:@"iPhone10,6"];
-    return isIPhoneX;
+//    struct utsname systemInfo;
+//    uname(&systemInfo);
+//    NSString *platform = [NSString stringWithCString:systemInfo.machine encoding:NSASCIIStringEncoding];
+//    if ([platform isEqualToString:@"i386"] || [platform isEqualToString:@"x86_64"]) {
+//        // judgment by height when in simulators
+//        return (CGSizeEqualToSize([UIScreen mainScreen].bounds.size, CGSizeMake(375, 812)) ||
+//                CGSizeEqualToSize([UIScreen mainScreen].bounds.size, CGSizeMake(812, 375)));
+//    }
+//    BOOL isIPhoneX = [platform isEqualToString:@"iPhone10,3"] || [platform isEqualToString:@"iPhone10,6"];
+//    return isIPhoneX;
+    return [SWPhoneInfo isIPhoneX];
 }
 + (CGFloat)navBarBottom {
     return [self isIphoneX] ? 88 : 64;
